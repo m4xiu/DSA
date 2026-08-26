b57f2c84422e568d80c0a4914b41dc05a7e812b3
Mathew <yogamathuzz@gmail.com>
2026-08-26 17:46:22 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (47.13%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -0,0 +1,29 @@
+class Solution:
+  def reverseKGroup(self, head: ListNode | None, k: int) -> ListNode | None:
+    if not head:
+      return None
+
+    tail = head
+
+    for _ in range(k):
+      if not tail:
+        return head
+      tail = tail.next
+
+    newHead = self._reverse(head, tail)
+    head.next = self.reverseKGroup(tail, k)
+    return newHead
+
+  def _reverse(
+      self,
+      head: ListNode | None,
+      tail: ListNode | None,
+  ) -> ListNode | None:
+    prev = None
+    curr = head
+    while curr != tail:
+      next = curr.next
+      curr.next = prev
+      prev = curr
+      curr = next
+    return prev
